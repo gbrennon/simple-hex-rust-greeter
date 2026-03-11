b1121ef33bce816446e299f46da5e763bf6ef71b
Glauber Brennon <glauberbrennon@gmail.com>
2026-03-11 02:46:37 -0300

    feat: remove sum_numbers functio   <type>(<scope>): <subject>

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,6 @@ fn greet(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
-fn sum_numbers(a: i32, b: i32) -> i32 {
-    a + b
-}
-
 fn main() {
     println!("{}", greet("world"));
     println!("{}", greet("Forgejo"));
@@ -29,20 +25,5 @@ mod tests {
     #[test]
     fn greet_uses_provided_rustoceans() {
         assert_eq!(greet("Forgejo"), "Hello, Forgejo!");
-    }
-
-    #[test]
-    fn sum_numbers_returns_correct_sum() {
-        assert_eq!(sum_numbers(2, 3), 5);
-    }
-
-    #[test]
-    fn sum_numbers_with_negative_values() {
-        assert_eq!(sum_numbers(-2, -3), -5);
-    }
-
-    #[test]
-    fn sum_numbers_with_mixed_values() {
-        assert_eq!(sum_numbers(-2, 3), 1);
     }
 }
