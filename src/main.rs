df128472fdd2ed492b1c33b686b3a2bf074c4b32
Glauber Brennon <glauberbrennon@gmail.com>
2026-03-11 15:09:56 -0300

    feat: impl GreeterApplicationService to use Greeter trait

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,55 @@ impl Greeter for SimpleGreeter {
     }
 }
 
+struct GreeterApplicationService {
+    greeter: Box<dyn Greeter>,
+}
+
+impl GreeterApplicationService {
+    fn new(greeter: Box<dyn Greeter>) -> Self {
+        Self { greeter }
+    }
+
+    fn execute(&self, name: &str) -> String {
+        self.greeter.greet(name)
+    }
+}
+
 fn main() {
-    println!("{}", Greeter.greet("world"));
-    println!("{}", Greeter.reet("Forgejo"));
-    println!("{}", Greeter.reet("Rustaceans"));
+    let greeter = SimpleGreeter{};
+    println!("{}", greeter.greet("world"));
+    println!("{}", greeter.greet("Forgejo"));
+    println!("{}", greeter.greet("Rustaceans"));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const greeter: Greeter = SimpleGreeter{};
+    #[test]
+    fn greet_service_when_world_then_returns_hello_world() {
+        let greeter = SimpleGreeter{};
+
+        let service = GreeterApplicationService::new(Box::new(greeter));
+
+        assert_eq!(service.execute("world"), "Hello, world!");
+    }
 
     #[test]
-    fn greet_returns_hello_name() {
-        assert_eq!(greeter.greet("world"), "Hello, world!");
+    fn greet_service_when_forgejo_then_returns_hello_forgejo() {
+        let greeter = SimpleGreeter{};
+
+        let service = GreeterApplicationService::new(Box::new(greeter));
+
+        assert_eq!(service.execute("Forgejo"), "Hello, Forgejo!");
+    }
+
+    #[test]
+    fn greet_service_when_rustaceans_then_returns_hello_rustaceans() {
+        let greeter = SimpleGreeter{};
+
+        let service = GreeterApplicationService::new(Box::new(greeter));
+
+        assert_eq!(service.execute("Rustaceans"), "Hello, Rustaceans!");
     }
 }
